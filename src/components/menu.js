68ffddbad36fa30fba9de0e057e06f0500b154cb
user <aevakatya@gmail.com>
2019-12-25 15:19:42 +0700

    Создан компонент статистики и методы для отрисовки стат данных, переключение между экранами статистики и доской задач

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,11 @@
 import AbstractComponent from './abstract-component.js';
 
+export const MenuItem = {
+  NEW_TASK: `control__new-task`,
+  STATISTICS: `control__statistic`,
+  TASKS: `control__task`,
+};
+
 export default class SiteMenu extends AbstractComponent {
   _createSiteMenuTemplate() {
     return (
@@ -30,6 +36,26 @@ export default class SiteMenu extends AbstractComponent {
 
   getTemplate() {
     return this._createSiteMenuTemplate();
+  }
+
+  setActiveItem(menuItem) {
+    const item = this.getElement().querySelector(`#${menuItem}`);
+
+    if (item) {
+      item.checked = true;
+    }
+  }
+
+  setOnChange(handler) {
+    this.getElement().addEventListener(`change`, (evt) => {
+      if (evt.target.tagName !== `INPUT`) {
+        return;
+      }
+
+      const menuItem = evt.target.id;
+
+      handler(menuItem);
+    });
   }
 }
 
